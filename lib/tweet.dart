6d57a3d276f3c970162c836c72168d41be3db357
Furcht968 <Tomu.iwanaga129+github@gmail.com>
2021-03-13 06:53:10 +0900

    add: ツイート画面に色々追加

--- lib/tweet.dart
+++ lib/tweet.dart
@@ -1,23 +1,80 @@
 import 'package:flutter/material.dart';
 
 class Tweet extends StatelessWidget {
+
+  TextEditingController _fieldController = TextEditingController();
+  bool _isEnabled = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Tweet"),
-        actions: [
-          RaisedButton(
-            child: const Text('Tweet'),
-            color: Colors.blue,
-            textColor: Colors.white,
-            shape: const StadiumBorder(),
-            onPressed: () {},
-          )
+      appBar: AppBar(title: Text("Tweet"), actions: [
+        RaisedButton(
+          child: const Text('Tweet'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: const StadiumBorder(),
+          onPressed: !_isEnabled==false ? null : () {},
+        )
+      ]),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: TextField(
+                autofocus: true,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "今何してる？",
+                ),
+                controller: _fieldController,
+                onChanged: (text){
+                  print(_isEnabled);
+                  if(text.length >= 140){
+                    _isEnabled = false;
+                  }else{
+                    _isEnabled = true;
+                  }
+                }
+              )
+            )
+          ),
+          Container(
+            child: Row(
+              children: [
+                // 画像追加
+                IconButton(
+                  icon: Icon(Icons.photo_library_outlined),
+                  tooltip: 'Picture',
+                  onPressed: () {},
+                ),
+                // GIF追加
+                IconButton(
+                  icon: Icon(Icons.gif_outlined),
+                  tooltip: 'Picture',
+                  onPressed: () {},
+                ),
+                // 投票追加
+                IconButton(
+                  icon: Icon(Icons.add_chart),
+                  tooltip: 'Picture',
+                  onPressed: () {},
+                ),
+                // 位置情報追加
+                IconButton(
+                  icon: Icon(Icons.add_location_alt_outlined),
+                  tooltip: 'Picture',
+                  onPressed: () {},
+                ),
+
+                // 文字数
+                
+              ]
+            ),
+            height: 40,
+          ),
         ]
-      ),
-      body: Center(
-        child: TextField(),
       ),
     );
   }
